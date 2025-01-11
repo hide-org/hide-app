46c350ffd463b7509e9a55113e08df4f91d87c22
Art Moskvin <amoskvin@me.com>
2025-01-11 14:50:10 +0100

    use copy webpack plugin

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,8 +1,9 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import * as path from 'path';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 require('dotenv').config();
 
@@ -17,4 +18,12 @@ export const plugins = [
     devServer: false
   }),
   new webpack.EnvironmentPlugin(['ANTHROPIC_API_KEY']),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'public'),
+        to: path.resolve(__dirname, '.webpack/renderer')
+      }
+    ]
+  })
 ];
