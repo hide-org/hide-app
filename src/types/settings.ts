71cb6eeface5d5f4514316c5a13931b7da3eafa6
Art Moskvin <amoskvin@me.com>
2025-02-02 16:16:53 +0100

    types: add utility functions for settings

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -18,3 +18,18 @@ export interface UserSettings {
     created_at: number;
     updated_at: number;
 }
+
+export const getCurrentProviderSettings = (settings: UserSettings): ProviderSettings | undefined => {
+    if (!settings.provider_settings[settings.model_provider]) {
+        return undefined;
+    }
+    return settings.provider_settings[settings.model_provider];
+};
+
+export const getCurrentProviderApiKey = (settings: UserSettings): string | undefined => {
+    const providerSettings = getCurrentProviderSettings(settings);
+    if (!providerSettings) {
+        return undefined;
+    }
+    return providerSettings.apiKey;
+};
