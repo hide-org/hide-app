d821839471d300fa38c8a390de1079797429184a
Anastasia Romanova <anromanova.design@gmail.com>
2025-02-11 21:29:02 +0100

    align menu

--- forge.config.ts
+++ forge.config.ts
@@ -50,11 +50,6 @@ const config: ForgeConfig = {
           },
         ],
       },
-      // Disable HMR to develop hide with hide
-      devServer: {
-        hot: false,
-        liveReload: false,
-      }
     }),
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -22,7 +22,7 @@ export function ModeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="start" alignOffset={0}>
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
