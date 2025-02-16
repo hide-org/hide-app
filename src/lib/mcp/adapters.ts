cea3a455bb02c661292d5ec3219d025ac40676eb
Art Moskvin <amoskvin@me.com>
2025-02-16 18:23:31 +0100

    remove unused stuff

--- src/lib/mcp/adapters.ts
+++ src/lib/mcp/adapters.ts
@@ -1,6 +1,5 @@
 import type { Tool as AnthropicTool } from '@anthropic-ai/sdk/resources/messages';
 import type { Tool as MCPTool, CallToolResult as MCPToolResult } from '@modelcontextprotocol/sdk/types';
-import { CoreTool, tool as aiTool, jsonSchema } from 'ai';
 
 export function mcpToAnthropicTool(tool: MCPTool): AnthropicTool {
     const { name, description, inputSchema, ...rest } = tool;
@@ -14,41 +13,4 @@ export function mcpToAnthropicTool(tool: MCPTool): AnthropicTool {
         },
         ...rest
     };
-}
-
-export function mcpToAiSdkTool(tool: MCPTool, callTool: (name: string, args: any) => Promise<MCPToolResult>): Record<string, CoreTool> {
-    const { name, description, inputSchema } = tool;
-    return {
-        [name]: aiTool({
-            description,
-            parameters: jsonSchema(inputSchema),
-            execute: async (args) => (callTool(name, args)),
-            // map to anthropic's tool result format:
-            experimental_toToolResultContent(result) {
-                if (typeof result === 'string') {
-                    return [{ type: 'text', text: result }];
-                }
-
-                if ('isError' in result && 'content' in result) {
-                    const r = result as MCPToolResult;
-                    return r.content.map(c => {
-                        if (c.type === 'text') {
-                            return { type: 'text', text: r.isError ? `Error: ${c.text}` : c.text };
-                        }
-
-                        if (c.type === 'image') {
-                            return { type: 'image', data: c.data, mimeType: c.mimeType };
-                        }
-
-                        if (c.type === 'resource') {
-                            console.error('Embedded resources are not supported');
-                        }
-                    });
-                }
-
-                console.error('Unexpected tool result:', result);
-                return [];
-            },
-        })
-    }
 }
