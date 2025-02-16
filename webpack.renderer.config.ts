ebd3f2bb9c0be41a555d07d39918c57b6297655b
Art Moskvin <amoskvin@me.com>
2025-02-16 21:53:56 +0100

    remove unused env vars

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,25 +1,14 @@
 import type { Configuration } from 'webpack';
 import * as path from 'path';
-import { DefinePlugin } from 'webpack';
-import dotenv from 'dotenv';
 
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
-
-// Load environment variables
-dotenv.config();
 
 export const rendererConfig: Configuration = {
   module: {
     rules,
   },
-  plugins: [
-    new DefinePlugin({
-      'process.env.ENABLE_ANALYTICS_TEST': JSON.stringify(process.env.ENABLE_ANALYTICS_TEST),
-      'process.env.POSTHOG_API_KEY': JSON.stringify(process.env.POSTHOG_API_KEY),
-    }),
-    ...plugins,
-  ],
+  plugins,
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
     alias: {
